e97bc038ab4fc31f144673d3e254f64e201f761c
Emrah Yurtlu <emrahyurtlu@hotmail.com>
2024-01-05 16:42:53 +0300

    Seed user

--- Backend/Data/ApplicationDbContext.cs
+++ Backend/Data/ApplicationDbContext.cs
@@ -2,29 +2,31 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Net8Identity.Data
+namespace Net8Identity.Data;
+
+public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
 {
-    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
-        {
-            
-        }
+        
+    }
 
-        public DbSet<ApplicationUser> ProdyumUsers { get; set; }
+    public DbSet<ApplicationUser> ProdyumUsers { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            var hasher = new PasswordHasher<ApplicationUser>();
-            builder.Entity<ApplicationUser>().HasData(
-                new ApplicationUser
-                {
-                    Email= "test@test.com",
-                    PasswordHash= hasher.HashPassword(null,"Password12*")
-                });
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        var hasher = new PasswordHasher<ApplicationUser>();
+        builder.Entity<ApplicationUser>().HasData(
+            new ApplicationUser
+            {
+                UserName = "test@test.com",
+                NormalizedUserName = "test@test.com".ToUpper(),
+                Email = "test@test.com",
+                NormalizedEmail = "test@test.com".ToUpper(),
+                PasswordHash = hasher.HashPassword(null,"Password12*"),
+            });
 
-            base.OnModelCreating(builder);
-        }
+        base.OnModelCreating(builder);
     }
 }
